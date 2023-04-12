70189f3f08ba39e379cd032a93bfeb38edd3452e
Samuel <samuel.b.rodrigues@accenture.com>
2023-04-12 13:48:45 -0300

    primary beans

--- src/test/java/sam/springbootproject/springbootproject/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/sam/springbootproject/springbootproject/controllers/ConstructorInjectedControllerTest.java
@@ -1,12 +1,8 @@
 package sam.springbootproject.springbootproject.controllers;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import sam.springbootproject.springbootproject.services.GreetingServiceImpl;
-
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ConstructorInjectedControllerTest {
 
